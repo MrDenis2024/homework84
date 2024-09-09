9f945bbe0ce726529fc2845c5e67dd24ca1d2af9
Khrunev Denis <deniskhrunev@gmail.com>
2024-09-09 12:20:02 +0600

    созадана структура регистрации и логина пользователя

--- todolist-api/routes/users.ts
+++ todolist-api/routes/users.ts
@@ -1,5 +1,52 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import User from '../models/User';
 
 const usersRouter = express.Router();
+
+usersRouter.post('/', async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    user.generateToken();
+    await user.save();
+
+    return res.send(user);
+  } catch (error) {
+    if(error instanceof mongoose.Error.ValidationError) {
+      return res.status(400).send(error);
+    }
+    return next(error);
+  }
+});
+
+usersRouter.post('/sessions', async (req, res, next) => {
+  try {
+    if(!req.body.username || !req.body.password) {
+      return res.status(400).send({error: 'Username and password required'});
+    }
+
+    const user = await User.findOne({username: req.body.username});
+
+    if(!user) {
+      return res.status(400).send({error: 'Invalid login or password'});
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if(!isMatch) {
+      return res.status(400).send({error: 'Invalid login or password'});
+    }
+
+    user.generateToken();
+    user.save();
+
+    return res.send(user);
+  } catch (error) {
+    return next(error);
+  }
+});
 
 export default usersRouter;
